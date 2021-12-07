e100dbb405e190797b6ef8a2808f90e73452ce9c
Alexander Sergeev <sergeev_ul73@mail.ru>
2021-12-07 17:11:38 +0400

    Модальный переход на экран добавления контакта
    * Готова верстка экрана (без корректного Title)
    * Закрытие экрана при клике на "Отменить"

---
+++ Contacts/View Controllers/ContactAddViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ContactAddViewController.swift
+//  Contacts
+//
+//  Created by Сергеев Александр on 07.12.2021.
+//
+
+import UIKit
+
+class ContactAddViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func cancelAddAction(_ sender: UIBarButtonItem) {
+        dismiss(animated: true)
+    }
+    
+}
