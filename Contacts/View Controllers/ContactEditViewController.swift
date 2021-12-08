0d6be6d5375dfbf5533b4d4ab416941ed25f9b45
Alexander Sergeev <sergeev_ul73@mail.ru>
2021-12-08 09:24:11 +0400

    Изменение контакта - готова верстка UI

---
+++ Contacts/View Controllers/ContactEditViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ContactEditViewController.swift
+//  Contacts
+//
+//  Created by Сергеев Александр on 08.12.2021.
+//
+
+import UIKit
+
+class ContactEditViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
