1a0d180e4c2e6f6a1b1be39bca6a2b701bc56c17
Alexander Sergeev <sergeev_ul73@mail.ru>
2021-12-08 13:24:29 +0400

    Изменение контакта - проверка ввода данных в поля

--- Contacts/View Controllers/ContactEditViewController.swift
+++ Contacts/View Controllers/ContactEditViewController.swift
@@ -17,17 +17,49 @@ class ContactEditViewController: UIViewController {
     // MARK: - Properties
     var getContacts: [Contact]?
     var currentContactId: Int?
+    var oldName: String?
+    var oldLastName: String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         if let contactId = currentContactId, let contact = getContacts?[contactId] {
+            oldName = contact.name
             contactName.text = contact.name
+            
+            oldLastName = contact.lastName
             contactLastName.text = contact.lastName
         }
+        
+        contactName.delegate = self
+        contactLastName.delegate = self
     }
 
     @IBAction func cancelEditAction(_ sender: Any) {
         dismiss(animated: false)
+    }
+}
+
+extension ContactEditViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        switch textField {
+        case contactName:
+            contactLastName.becomeFirstResponder()
+        case contactLastName:
+            contactName.becomeFirstResponder()
+        default:
+            break
+        }
+        
+        return true
+    }
+    
+    func textFieldDidChangeSelection(_ textField: UITextField) {
+        doneButton.isEnabled = false
+        
+        guard contactName.text != oldName || contactLastName.text != oldLastName else { return }
+        guard contactName.text != "" || contactLastName.text != "" else { return }
+        
+        doneButton.isEnabled = true
     }
 }
